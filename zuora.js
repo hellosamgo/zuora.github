8dc0554cb8e9ec4ff97903f28014a122d67d80ed
Sam Go <samgo@new-host-2.home>
2014-01-05 21:25:08 -0500

    First commit

---
+++ zuora.js
@@ -0,0 +1,54 @@
+var request = require('request'),
+    config = require('./config');
+
+console.log(process.argv);
+
+var makeConnection = function(callback){
+  var connectUrl = 'https://api.zuora.com/rest/v1/connections';
+  var connectHeaders = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'apiAccessKeyId': config.access_key,
+      'apiSecretAccessKey': config.secret_key
+    };
+
+  request.post({ url: connectUrl, headers: connectHeaders}, function (err, res, body) {
+    if (err) return console.error(err);
+    if (res.statusCode !== 200) return console.error(res.body);
+
+    var cookie = res.headers['set-cookie'][0].split(';')[0];
+    callback(cookie);
+  });
+}
+
+var getAccount = function(cookie, accountId, callback) {
+  var getUrl = 'https://api.zuora.com/rest/v1/accounts/' + accountId;
+
+  var headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Cookie': cookie
+    };
+
+  request.get({ url: getUrl, headers: headers}, function (err, res, body) {
+    if (err) return console.error(err);
+    if (res.statusCode !== 200) return console.error(res.body);
+
+    var data = JSON.parse(body);
+    callback(data, accountId);
+  });
+}
+
+var accounts = ['52336', '16658', '16698']
+
+makeConnection(function(cookie){
+  for (var i = 0; i < accounts.length; i++) {
+    getAccount(cookie, accounts[i], function(data, account){
+      if (data.success) {
+        console.log("Account :", account, " MRR: $", data.metrics.contractedMrr);
+      } else {
+        console.log("Could not find Account: ",  account);
+      }
+    });
+  };
+});
